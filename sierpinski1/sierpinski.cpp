e00a2d62dbb4ebfa1a45450211a5ceb0371d178f
john crimmins <john.crimmins@gmail.com>
2011-01-30 12:28:05 -0600

    Initial commit for sierpinski demo.

---
+++ sierpinski1/sierpinski.cpp
@@ -0,0 +1,88 @@
+#include<OpenGl/Gl.h>
+#include<OpenGl/Glu.h>
+#include<GLUT/glut.h>
+#include<iostream>
+
+class GLIntPoint{
+public:
+  GLint x, y;
+};
+
+void myInit();
+void myDisplay();
+void myReshape(int, int);
+void myMouse(int, int, int,  int);
+void myKeyboard(unsigned char , int , int);
+void drawDot(GLIntPoint p);
+void drawSierpinski();
+
+int main(int argc, char** argv){
+
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(640,480);
+  glutInitWindowPosition(100,150);
+  glutCreateWindow("My first attempt!");
+
+  glutDisplayFunc(myDisplay);
+  glutReshapeFunc(myReshape);
+  glutMouseFunc(myMouse);
+  glutKeyboardFunc(myKeyboard);
+
+  myInit();
+
+  glutMainLoop();
+  return 0;
+}
+void myInit(){
+  glClearColor(0.0,0.0,0.0,0.0);
+  glColor3f(1.0f, 1.0f, 1.0f);
+  glPointSize(1.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+}
+
+void myDisplay(){
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_POINTS);
+
+  drawSierpinski();
+
+  glEnd();
+  glFlush();
+}
+
+void drawSierpinski(){
+  const int length = 3;
+  GLIntPoint points[length] = {{10,10},{300,10},{300,300}};
+  int index = random() % length;
+  GLIntPoint point = points[index];
+
+  drawDot(point);
+  for(int i =0; i<10000; i++){
+    index = random() % length;
+    point.x = (point.x + points[index].x)/2;
+    point.y = (point.y + points[index].y)/2;
+    drawDot(point);
+  }
+}
+
+void drawDot(GLIntPoint p){
+  glBegin(GL_POINTS);
+  glVertex2i(p.x, p.y);
+  glEnd();
+}
+
+void myReshape(int i, int j){
+
+}
+
+void myMouse(int button, int state, int x, int y){
+
+}
+
+void myKeyboard(unsigned char c, int i, int j){
+
+}
+
